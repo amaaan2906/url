d4c9831e1df4eaec0c1ce0b649647fe0f80dd522
Aman Tawakley <tawakley.aman@gmail.com>
2021-05-08 17:37:11 -0400

    Minor change

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,22 +1,22 @@
 <template>
-	<div class="home">
-		<header>
-			<h1>Shortener</h1>
-		</header>
-	</div>
+  <div class="home">
+    <header>
+      <h1>4yd</h1>
+    </header>
+  </div>
 </template>
 
 <script>
-	// @ is an alias to /src
-	export default {
-		name: "Home",
-		components: {},
-		data() {
-			return {
-				lightMode: false,
-			};
-		},
-	};
+// @ is an alias to /src
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      lightMode: false
+    };
+  }
+};
 </script>
 
 <style scoped></style>
